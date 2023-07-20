73a55b75d063a7b79a651c6f67325f78b5b71db6
wknowpark797 <wknowpark797@gmail.com>
2023-07-20 11:58:59 +0900

    컴포넌트 내부 styled 위치 수정

--- client/src/common/Header.js
+++ client/src/common/Header.js
@@ -1,33 +1,33 @@
 import { NavLink, Link } from 'react-router-dom';
 import styled from 'styled-components';
 
+const HeaderWrap = styled.header`
+	width: 350px;
+	height: 100vh;
+	background: #222;
+	position: fixed;
+	top: 0;
+	left: 0;
+	padding: 50px;
+`;
+const Logo = styled.h1`
+	margin-bottom: 40px;
+	a {
+		font: 40px/1 'arial';
+		color: #fff;
+	}
+`;
+const Gnb = styled.ul`
+	a {
+		display: block;
+		padding: 10px;
+		font: bold 16px/1 'arial';
+		color: #bbb;
+	}
+`;
+
 function Header() {
 	const activeStyle = { color: 'hotpink' };
-
-	const HeaderWrap = styled.header`
-		width: 350px;
-		height: 100vh;
-		background: #222;
-		position: fixed;
-		top: 0;
-		left: 0;
-		padding: 50px;
-	`;
-	const Logo = styled.h1`
-		margin-bottom: 40px;
-		a {
-			font: 40px/1 'arial';
-			color: #fff;
-		}
-	`;
-	const Gnb = styled.ul`
-		a {
-			display: block;
-			padding: 10px;
-			font: bold 16px/1 'arial';
-			color: #bbb;
-		}
-	`;
 
 	return (
 		<HeaderWrap>
--- client/src/common/Layout.js
+++ client/src/common/Layout.js
@@ -1,49 +1,49 @@
 import styled from 'styled-components';
 
-function Layout({ children, name }) {
-	const MainWrap = styled.main`
-		width: calc(100% - 350px);
-		min-height: 100vh;
-		float: right;
+const MainWrap = styled.main`
+	width: calc(100% - 350px);
+	min-height: 100vh;
+	float: right;
 
-		> .inner {
-			width: 100%;
-			padding: 60px;
+	> .inner {
+		width: 100%;
+		padding: 60px;
 
-			h1 {
-				font: normal 40px/1 'arial';
-				color: #333;
-				margin-bottom: 30px;
+		h1 {
+			font: normal 40px/1 'arial';
+			color: #333;
+			margin-bottom: 30px;
+		}
+
+		section {
+			label {
+				display: block;
+				font: 16px/1 'arial';
+				color: #555;
+				margin-bottom: 5px;
 			}
-
-			section {
-				label {
-					display: block;
-					font: 16px/1 'arial';
-					color: #555;
-					margin-bottom: 5px;
-				}
-				input[type='text'],
-				textarea {
-					width: 50%;
-					padding: 5px 8px;
-					border: 1px solid #999;
-					margin-bottom: 20px;
-					resize: none;
-					display: block;
-				}
-				button {
-					display: inline-block;
-					padding: 5px 20px;
-					background: #555;
-					color: #fff;
-					border: none;
-					cursor: pointer;
-				}
+			input[type='text'],
+			textarea {
+				width: 50%;
+				padding: 5px 8px;
+				border: 1px solid #999;
+				margin-bottom: 20px;
+				resize: none;
+				display: block;
+			}
+			button {
+				display: inline-block;
+				padding: 5px 20px;
+				background: #555;
+				color: #fff;
+				border: none;
+				cursor: pointer;
 			}
 		}
-	`;
+	}
+`;
 
+function Layout({ children, name }) {
 	return (
 		<MainWrap className={`content ${name}`}>
 			<div className='inner'>
