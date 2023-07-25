1220e70afb0230bd7d407287669bd3f4a6539b20
wknowpark797 <wknowpark797@gmail.com>
2023-07-25 12:51:13 +0900

    serializableCheck 에러 처리

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import userSlice from './redux/userSlice';
 
 const store = configureStore({
 	reducer: {
 		user: userSlice,
 	},
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
 });
 
 ReactDOM.render(
